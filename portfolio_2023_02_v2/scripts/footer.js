9371cf2f49b9f89cb4cdf59057ed971e506526cc
Bete.mrt <bete.mrt@gmail.com>
2024-02-16 19:18:20 -0300

    :recycle: refactor: Add header.js

--- portfolio_2023_02_v2/scripts/footer.js
+++ portfolio_2023_02_v2/scripts/footer.js
@@ -3,4 +3,4 @@ const footerContent = `
     <p><a target="_blank" href="https://icons8.com/icon/T12wf6pMSu6F/horda-de-world-of-warcraft">World of Warcraft Horde</a> icon by <a target="_blank" href="https://icons8.com">Icons8</a></p>
     `;
 
-  document.getElementById('footer-container').innerHTML = footerContent;
+  document.getElementById('footer__container').innerHTML = footerContent;
