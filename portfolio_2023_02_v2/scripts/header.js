ea1eddac2503908265e0b0a9f68b79bb3fa17dc4
Elisabete-MO <bete.mrt@gmail.com>
2025-12-04 22:10:16 -0300

    :recycle: refactor: Corrigir link de estilo e atualizar conteúdo do cabeçalho

--- portfolio_2023_02_v2/scripts/header.js
+++ portfolio_2023_02_v2/scripts/header.js
@@ -5,13 +5,11 @@ const headerContent = `
   <ul>
     <li><a href="#anc_post" class="header__top__link">Home</a></li>
     <li><a href="#" class="header__top__link">About me</a></li>
-    <li><a href="#" class="header__top__link">Contact</a></li>
     <li><a href="#" class="header__top__link">Formacao</a></li>
     <li><a href="#" class="header__top__link">Experiencia</a></li>
     <li><a href="#" class="header__top__link">Projetos</a></li>
     <li><a href="#" class="header__top__link">Skills</a></li>
-    <li><a href="#" class="header__top__link">Contato</a></li>
-    <li><a href="#" class="header__top__link">Curriculo</a></li>
+    <li><a href="#" class="header__top__link">Contact</a></li>
   </ul>
 </nav> `;
 
