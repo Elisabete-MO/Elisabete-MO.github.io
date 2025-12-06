487467edf7a842aa570f6fbd4e2ab9d79378fb1a
Elisabete-MO <bete.mrt@gmail.com>
2025-12-06 12:40:58 -0300

    :recycle: refactor: Atualizar estrutura do cabeçalho no header.js

--- portfolio_2023_02_v2/scripts/header.js
+++ portfolio_2023_02_v2/scripts/header.js
@@ -1,18 +1,13 @@
 const headerContent = `
 <img src="./images/devil.jpeg" alt=":P" width="100px" height="100px">
 <h1>My-Name</h1>
-<nav>
-  <ul>
-    <li><a href="#anc_post" class="header__top__link">Home</a></li>
-    <li><a href="#" class="header__top__link">About me</a></li>
-    <li><a href="#" class="header__top__link">Contact</a></li>
-    <li><a href="#" class="header__top__link">Formacao</a></li>
-    <li><a href="#" class="header__top__link">Experiencia</a></li>
-    <li><a href="#" class="header__top__link">Projetos</a></li>
-    <li><a href="#" class="header__top__link">Skills</a></li>
-    <li><a href="#" class="header__top__link">Contato</a></li>
-    <li><a href="#" class="header__top__link">Curriculo</a></li>
-  </ul>
-</nav> `;
+  <nav class="nav" role="navigation" aria-label="Main">
+      <ul>
+          <li><a href="#about" class="header__top__link">About</a></li>
+          <li><a href="#certs" class="header__top__link">Certifications</a></li>
+          <li><a href="#skills" class="header__top__link">Skills</a></li>
+          <li><a href="#projects" class="header__top__link">Projects</a></li>
+      </ul>
+  </nav> `;
 
 document.getElementsByClassName('header__top')[0].innerHTML = headerContent;
