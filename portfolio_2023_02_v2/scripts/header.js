e3d38aa1a5c3796ed1974c5cf6b145e0234da6d3
Elisabete-MO <bete.mrt@gmail.com>
2025-12-15 14:40:02 -0300

    :recycle: refactor: Atualizar a estrutura da lista de navegação no header.js

--- portfolio_2023_02_v2/scripts/header.js
+++ portfolio_2023_02_v2/scripts/header.js
@@ -2,12 +2,12 @@ const headerContent = `
 <img src="./images/devil.jpeg" alt=":P" width="100px" height="100px">
 <h1>My-Name</h1>
   <nav class="nav" role="navigation" aria-label="Main">
-      <ul>
-          <li><a href="#about" class="header__top__link">About</a></li>
-          <li><a href="#certs" class="header__top__link">Certifications</a></li>
-          <li><a href="#skills" class="header__top__link">Skills</a></li>
-          <li><a href="#projects" class="header__top__link">Projects</a></li>
-      </ul>
+    <ul>
+      <li><a href="#home" class="header__top__link">Home</a></a></li>
+      <li><a href="#about" class="header__top__link">About</a></li>
+      <li><a href="#skills" class="header__top__link">Skills</a></li>
+      <li><a href="#projects" class="header__top__link">Projects</a></li>
+    </ul>
   </nav> `;
 
 document.getElementsByClassName('header__top')[0].innerHTML = headerContent;
