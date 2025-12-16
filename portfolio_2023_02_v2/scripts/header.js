8841cea2d527efbda90670171d34d82cb42c1cea
Elisabete-MO <bete.mrt@gmail.com>
2025-12-16 13:17:04 -0300

    :art: add: Adicionar seção de habilidades com conteúdo e estilo; atualizar links de navegação e incluir novo arquivo de script para habilidades

--- portfolio_2023_02_v2/scripts/header.js
+++ portfolio_2023_02_v2/scripts/header.js
@@ -5,8 +5,8 @@ export function loadHeader() {
       <ul>
         <li><a href="#home">Home</a></li>
         <li><a href="#about">About</a></li>
-        <li><a href="#skills">Skills</a></li>
         <li><a href="#projects">Projects</a></li>
+        <li><a href="#skills">Skills</a></li>
       </ul>
     </nav>
   `;
--- portfolio_2023_02_v2/scripts/main.js
+++ portfolio_2023_02_v2/scripts/main.js
@@ -1,5 +1,8 @@
 import { loadHeader } from './header.js';
 import { initClock } from './clock.js';
+import { loadSkills } from './skills.js';
+
+loadSkills();
 
 loadHeader();
 initClock();
